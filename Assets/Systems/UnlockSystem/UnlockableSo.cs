b703c2dc678284bdaa0092fc6ce713dd258b3063
AntoninJuquel <antonin.juquel@outlook.fr>
2021-04-15 11:02:00 +0200

    Added UnlockSystem

---
+++ Assets/Systems/UnlockSystem/UnlockableSo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Systems.UnlockSystem
+{
+    public class UnlockableSo : ScriptableObject
+    {
+        [SerializeField] private int value;
+        [SerializeField] private int levelRequired;
+        private UnlockStatus _status = UnlockStatus.Locked;
+
+        private enum UnlockStatus
+        {
+            Locked,
+            Unlockable,
+            Unlocked
+        }
+
+        public void UpdateStatus(VaultSo vaultSo)
+        {
+            if (_status != UnlockStatus.Locked) return;
+            if (vaultSo.GetLevel() >= levelRequired)
+                _status = UnlockStatus.Unlockable;
+        }
+
+        public void Unlock(VaultSo vaultSo)
+        {
+            if (_status != UnlockStatus.Unlockable) return;
+            var res = vaultSo.GetValue() - value;
+            if (res <= 0) return;
+            _status = UnlockStatus.Unlocked;
+            vaultSo.SetValue(res);
+        }
+    }
+}
---
+++ Assets/Systems/UnlockSystem/VaultSo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Systems.UnlockSystem
+{
+    public class VaultSo : ScriptableObject
+    {
+        [SerializeField] private int value;
+        [SerializeField] private int level;
+
+        public int GetValue() => value;
+        public int GetLevel() => level;
+
+        public void SetValue(int newValue) => value = newValue;
+        public void SetLevel(int newLevel) => level = newLevel;
+
+        public void AddValue(int amount) => value += amount;
+        public void AddLevel(int amount) => level += amount;
+    }
+}
