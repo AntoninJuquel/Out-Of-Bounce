31d6c77308918e518fa5b7764d872591dc974531
AntoninJuquel <antonin.juquel@outlook.fr>
2021-04-15 22:26:54 +0200

    Moved PlayerSo Editor

--- Assets/Scripts/PlayerSo.cs
+++ Assets/Scripts/PlayerSo.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using Systems.AchievementSystem;
+﻿using Systems.AchievementSystem;
 using Systems.UnlockSystem;
 using UnityEngine;
+using UnityEditor;
+using System.Globalization;
 
 
 [CreateAssetMenu(fileName = "New player data", menuName = "Player", order = 0)]
@@ -11,4 +11,43 @@ public class PlayerSo : ScriptableObject
     [SerializeField] private Vault vault = new Vault();
     [SerializeField] private Achievements achievements = new Achievements();
     public Achievements GetAchievements() => achievements;
+}
+
+[CustomEditor(typeof(PlayerSo))]
+public class PlayerSoEditor : Editor
+{
+    private bool _foldout;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var player = (PlayerSo) target;
+        var achievements = player.GetAchievements().GetDictionary();
+        if (GUILayout.Button("Show"))
+            _foldout = !_foldout;
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (!_foldout) return;
+        foreach (var kvp in achievements)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(kvp.Key.ToString(), GUILayout.Width(100));
+
+            EditorGUILayout.BeginVertical();
+            DrawProperty("Best", kvp.Value.GetBest());
+            DrawProperty("Last", kvp.Value.GetLast());
+            DrawProperty("Total", kvp.Value.GetTotal());
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        }
+    }
+
+    private void DrawProperty(string label, float value)
+    {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(label, GUILayout.Width(100));
+        EditorGUILayout.LabelField(value.ToString(CultureInfo.InvariantCulture));
+        EditorGUILayout.EndHorizontal();
+    }
 }
