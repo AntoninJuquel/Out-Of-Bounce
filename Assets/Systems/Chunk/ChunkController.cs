a57903d1afda309389bb51c02ffb563efc8a9f74
AntoninJuquel <antonin.juquel@outlook.fr>
2021-04-16 12:13:20 +0200

    Update ChunkController.cs

--- Assets/Systems/Chunk/ChunkController.cs
+++ Assets/Systems/Chunk/ChunkController.cs
@@ -4,14 +4,14 @@ namespace Systems.Chunk
 {
     public class ChunkController : MonoBehaviour
     {
-        [SerializeField] private Chunk chunk;
         private ChunkManager _chunkManager;
+        private Chunk _chunk;
         private Transform _transform;
 
         private void Awake()
         {
             _transform = transform;
-            chunk = null;
+            _chunk = null;
         }
 
         private void Start()
@@ -23,9 +23,9 @@ namespace Systems.Chunk
         {
             if(!_chunkManager.started) return;
             var position = _transform.position;
-            chunk ??= _chunkManager.GetClosestChunk(this, position, position);
-            if (chunk.GetBounds().Contains(position)) return;
-            chunk = _chunkManager.GetClosestChunk(this, chunk.GetBounds().center, position);
+            _chunk ??= _chunkManager.GetClosestChunk(this, position, position);
+            if (_chunk.GetBounds().Contains(position)) return;
+            _chunk = _chunkManager.GetClosestChunk(this, _chunk.GetBounds().center, position);
         }
     }
 }
