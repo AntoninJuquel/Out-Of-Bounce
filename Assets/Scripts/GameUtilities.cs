58bc582c76b400c15057774391442e68ca068681
AntoninJuquel <antonin.juquel@outlook.fr>
2021-04-16 21:40:48 +0200

    Platform and GU

---
+++ Assets/Scripts/GameUtilities.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+public static class Utilities
+{
+    public static Vector2[] ToVector2Array(Vector3[] vector3Array) => System.Array.ConvertAll(vector3Array, (v3) => new Vector2(v3.x, v3.y));
+}
