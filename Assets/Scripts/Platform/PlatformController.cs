b2ebe6c9eec5b289db240ff4ffd9b60a1f494662
AntoninJuquel <antonin.juquel@outlook.fr>
2021-04-16 21:40:23 +0200

    Added platform logic

---
+++ Assets/Scripts/Platform/PlatformController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Platform
+{
+    public class PlatformController : MonoBehaviour, ICollide
+    {
+        private LineRenderer _lineRenderer;
+        private EdgeCollider2D _edgeCollider;
+        private float Length => Vector2.Distance(_lineRenderer.GetPosition(0), _lineRenderer.GetPosition(1));
+
+        private void Awake()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+            _edgeCollider = GetComponent<EdgeCollider2D>();
+        }
+
+        private void Disable()
+        {
+            //soundFXChannel.RaiseEvent("PlatformActivate");
+            _edgeCollider.enabled = false;
+            gameObject.SetActive(false);
+        }
+
+        public void Activate(float time)
+        {
+            //soundFXChannel.RaiseEvent("PlatformActivate");
+            _edgeCollider.enabled = true;
+            Invoke("Disable", time);
+        }
+
+        public void Bounce(GameObject ball, float bouncyness)
+        {
+            var rigid = ball.GetComponent<Rigidbody2D>();
+            rigid.velocity = rigid.velocity.normalized * (bouncyness * (1 + 1 / Length));
+        }
+
+        public LineRenderer GetLineRenderer() => _lineRenderer;
+        public EdgeCollider2D GetEdgeCollider2D() => _edgeCollider;
+        public void SetActive(bool active) => gameObject.SetActive(active);
+    }
+}
---
+++ Assets/Scripts/Platform/PlatformManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using Systems.Pool;
+using UnityEngine;
+
+namespace Platform
+{
+    public class PlatformManager : ObjectPool
+    {
+        [SerializeField] private int platformAmount = 3, platformCounter = 3;
+        [SerializeField] private float radius = .25f, platformTimer = 5f;
+        private PlatformController _currentPlatform;
+        private List<Vector3> _mousePositions = new List<Vector3>();
+        private Vector2 MousePosition => _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        private Camera _mainCamera;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            platformCounter = platformAmount;
+        }
+
+        private void Update()
+        {
+            if (GameManager.GameStatus == GameStatus.Paused) return;
+
+            if (Input.GetMouseButtonDown(0) && platformCounter > 0)
+            {
+                StartLine();
+            }
+
+            if (!_currentPlatform) return;
+
+            if (Input.GetMouseButton(0))
+            {
+                UpdateLine();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                _currentPlatform.Activate(platformTimer);
+                ExitLine();
+            }
+        }
+
+        private void DrawLine()
+        {
+            var lr = _currentPlatform.GetLineRenderer();
+            var edgeCol = _currentPlatform.GetEdgeCollider2D();
+            lr.positionCount = _mousePositions.Count;
+            lr.SetPositions(_mousePositions.ToArray());
+            edgeCol.points = Utilities.ToVector2Array(_mousePositions.ToArray());
+        }
+
+        private void StartLine()
+        {
+            platformCounter--;
+            // UpdatePlatformCounter();
+            _mousePositions.Add(MousePosition);
+            _currentPlatform = SpawnFromPool("Platform", Vector2.zero, Quaternion.identity).GetComponent<PlatformController>();
+            _currentPlatform.SetActive(true);
+            var lr = _currentPlatform.GetLineRenderer();
+            var edgeCol = _currentPlatform.GetEdgeCollider2D();
+            _mousePositions.Add(MousePosition);
+            lr.widthMultiplier = edgeCol.edgeRadius = radius;
+            edgeCol.enabled = false;
+            DrawLine();
+        }
+
+        private void UpdateLine()
+        {
+            if (_mousePositions.Count < 2)
+            {
+                _mousePositions.Add(MousePosition);
+            }
+            else
+            {
+                _mousePositions[1] = MousePosition;
+            }
+
+            DrawLine();
+        }
+
+        private void ExitLine()
+        {
+            _mousePositions = new List<Vector3>();
+            _currentPlatform = null;
+        }
+
+        // private void UpdatePlatformCounter() => platformsCounterChannel.RaiseEvent(string.Concat(platformCounter, " |"));
+        public void ResupplyPlatforms(float points)
+        {
+            if (points < 0)
+                platformCounter = 0;
+            else if (points > 0)
+                platformCounter = platformAmount;
+            // UpdatePlatformCounter();
+        }
+    }
+}
