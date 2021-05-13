e684ba2b5625d758ef17219fc1156c6607a58a7f
AntoninJuquel <antonin.juquel@outlook.fr>
2021-05-14 00:23:25 +0200

    Added stop dot

---
+++ Assets/Scripts/Dot/DotSos/StopDot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Dot.DotSos
+{
+    [CreateAssetMenu(fileName = "New stop dot", menuName = "Dots/Stop", order = 0)]
+    public class StopDot : DotSo
+    {
+        public override void Bounce(GameObject ball, GameObject dot, float bouncyness)
+        {
+            base.Bounce(ball, dot, 0);
+        }
+    }
+}
