9f29fcd7bb59d58ae2f219c1630dabffa3730f0e
AntoninJuquel <antonin.juquel@outlook.fr>
2021-05-27 01:22:55 +0200

    Fix explosive dot

--- Assets/Scripts/Dot/DotSos/ExplosiveDot.cs
+++ Assets/Scripts/Dot/DotSos/ExplosiveDot.cs
@@ -9,15 +9,19 @@ namespace Dot.DotSos
         [SerializeField] private float force = 2f, radius = 15f;
         [SerializeField] protected LayerMask whatIsDot, whatIsBall;
 
-        public override void Bounce(GameObject ball, GameObject dot, float bouncyness)
-        {
-            base.Bounce(ball, dot, bouncyness * force);
-        }
-
         public override void Destroy(GameObject dot)
         {
+            var dotPosition = dot.transform.position;
+
+            foreach (var col in Physics2D.OverlapCircleAll(dotPosition, radius, whatIsBall))
+            {
+                var direction = col.transform.position - dotPosition;
+                col.GetComponent<Rigidbody2D>().velocity += (Vector2) direction.normalized * force;
+                Debug.Log(col.name);
+            }
+
             base.Destroy(dot);
-            foreach (var col in Physics2D.OverlapCircleAll(dot.transform.position, radius, whatIsDot).Where(c => c.gameObject != dot.gameObject))
+            foreach (var col in Physics2D.OverlapCircleAll(dotPosition, radius, whatIsDot).Where(c => c.gameObject != dot.gameObject))
             {
                 if (!col.gameObject.activeSelf) continue;
                 col.GetComponent<DotController>()?.Destroy();
