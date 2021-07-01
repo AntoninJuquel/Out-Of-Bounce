b233f79ca3cc923fb66cfad61a954b0033bc57d9
AntoninJuquel <antonin.juquel@outlook.fr>
2021-07-01 11:20:28 +0200

    Added rewind dot

---
+++ Assets/Scripts/Dot/DotSos/RewindDot.cs
@@ -0,0 +1,15 @@
+﻿using Systems.SpaceTime;
+using UnityEngine;
+
+namespace Dot.DotSos
+{
+    [CreateAssetMenu(fileName = "New rewind dot", menuName = "Dots/Rewind", order = 0)]
+    public class RewindDot : DotSo
+    {
+        public override void Bounce(GameObject ball, GameObject dot, float bouncyness)
+        {
+            base.Bounce(ball, dot, 0);
+            ball.GetComponent<TimeBody>().StartRewind();
+        }
+    }
+}
---
+++ Assets/Systems/SpaceTime/TimeBody.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Systems.SpaceTime
+{
+    public class TimeBody : MonoBehaviour
+    {
+        [SerializeField] private List<PositionRotation> positionRotations = new List<PositionRotation>();
+        private Transform _transform;
+        private Rigidbody2D _rb;
+        private bool _isRewinding;
+
+        private void Awake()
+        {
+            _transform = transform;
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (_isRewinding) Rewind();
+        }
+
+        private void FixedUpdate()
+        {
+            
+            if (!_isRewinding) Record();
+        }
+
+        private void Record()
+        {
+            if (positionRotations.Count > Mathf.Round(5f / Time.fixedDeltaTime))
+            {
+                positionRotations.RemoveAt(positionRotations.Count - 1);
+            }
+
+            positionRotations.Insert(0, new PositionRotation(_transform.position, _transform.rotation));
+        }
+
+        private void Rewind()
+        {
+            if (positionRotations.Count > 0)
+            {
+                _transform.SetPositionAndRotation(positionRotations[0].Position, positionRotations[0].Rotation);
+                positionRotations.RemoveAt(0);
+            }
+            else
+            {
+                StopRewind();
+            }
+        }
+
+        public void StartRewind()
+        {
+            _isRewinding = true;
+            if (_rb) _rb.isKinematic = true;
+        }
+
+        public void StopRewind()
+        {
+            _isRewinding = false;
+            if (_rb) _rb.isKinematic = false;
+        }
+    }
+
+    [Serializable]
+    public class PositionRotation
+    {
+        public Vector3 Position { get; }
+        public Quaternion Rotation { get; }
+
+        public PositionRotation(Vector3 position, Quaternion rotation)
+        {
+            this.Position = position;
+            this.Rotation = rotation;
+        }
+    }
+}
